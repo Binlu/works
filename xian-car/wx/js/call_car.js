760d69aa498a7af29120fa16a9a6445e0bb59f36
unknown <826202369@qq.com>
2016-03-18 17:15:31 +0800

    2016-3-18

--- xian-car/wx/js/call_car.js
+++ xian-car/wx/js/call_car.js
@@ -1,4 +1,122 @@
+// 滚动变量
+var myScroll1,myScroll2,myScroll3,myScroll4,myScroll5,myScroll6,myScroll7,myScroll8,myScroll9,myScroll10,myScroll11,myScroll12,myScroll13,myScroll14,myScroll15,myScroll16,myScroll17,myScroll18,myScroll19;
+// 自定义方法
+var $m={
+    // 分享链接
+    share_href: window.location.href,
+    // 搜索数据
+    s_arr1:[],
+    s_arr2:[],
+    // 图片地址前缀
+    img_url:"images/",
+    // 当前页面
+    active_scroll:1,
+    // 重新布局
+    rs: function(){
+        var a=this;
+        var limit=720;
+        var dw=document.documentElement.clientWidth;
+        var dh=document.documentElement.clientHeight;
+        var fw=dw>limit?limit:dw;
+        $("body").css({"font-size":1*fw/360+"em"});
+        if(dw>limit){
+            $(".center_box").css({"left":(dw-limit)/2+"px"});
+        };
+        $(".circular_div").each(function(){
+            a.setHeight($(this),1)
+        });
+        
+        
+        a.refreshPage(a.active_scroll);
+    },
+    // 刷新scroll
+    refreshPage: function(type){
+        if(type==1){
+            myScroll1.refresh();
+        }else if(type==2){
+            myScroll2.refresh();
+        }else if(type==3){
+            myScroll3.refresh();
+        }else if(type==4){
+            myScroll4.refresh();
+        }else if(type==5){
+            myScroll5.refresh();
+        }else if(type==6){
+            myScroll6.refresh();
+        }else if(type==7){
+            myScroll7.refresh();
+        }else if(type==8){
+            myScroll8.refresh();
+        }else if(type==9){
+            myScroll9.refresh();
+        }else if(type==10){
+            myScroll10.refresh();
+        }else if(type==11){
+            myScroll11.refresh();
+        }else if(type==12){
+            myScroll12.refresh();
+        }else if(type==13){
+            myScroll13.refresh();
+        }else if(type==14){
+            myScroll14.refresh();
+        }
+    },
+    // ajax 请求地址
+    ajax_link: ["deal_info","is_set_info"],
+    // 居中显示
+    toCenter:function(obj,par){
+        var dh=par.height();
+        var oh=obj.outerHeight();
+        obj.css({"margin-top": (dh-oh)/2-50+"px"});
+    },
+    // 图片加载
+    img_arr:[
+        "images/1.jpg",
+        "images/2.jpg",
+        "images/3.jpg",
+        "images/4.jpg",
+        "images/5.jpg",
+        "images/7.jpg",
+        "images/8.jpg",
+        "images/icon1.png",
+        "images/icon2.png",
+        "images/prod.png",
+        "images/prod1.jpg",
+        "images/txt_pic1.png",
+        "images/txt_pic2.png",
+        "images/txt_pic3.png",
+        "images/txt_pic4.png",
+        "images/txt_pic5.png",
+        "images/txt_pic6.png",
+        "images/txt_pic7.png",
+        "images/txt_pic8.png",
+    ],
+    // 设置宽高
+    setHeight:function(obj,k){
+        var ow=obj.width();
+        console.log(ow);
+        var num=parseInt(ow*k);
+        obj.height(num+"px");
+    }
+}
 $(function(){
+	// 绑定滚动
+    myScroll1=new IScroll('.page1',{mouseWheel: true,hideScrollbar: true,click: true,bounce:false});
+    myScroll2=new IScroll('.list',{mouseWheel: true,hideScrollbar: true,click: true});
+    myScroll2=new IScroll('.page3',{mouseWheel: true,hideScrollbar: true,click: true});
+    
+
+    $m.rs();
+    
+	// 存储搜索数据
+	(function(){
+		var arr2=$(".js_city_search").attr("data-id")?$(".js_city_search").attr("data-id"):[];
+		arr2=eval(arr2);
+		if(arr2.length>0){
+			$m.s_arr2=save_search(arr2,2);
+		}
+		
+	})();
 	searchbg2=$(".js_city_search").css("background-image");
 	var new_city=new ms();
 	new_city.local();
@@ -11,17 +129,17 @@ $(function(){
 		var txt=$(this).val();
 		if($(this).val()==""||$(this).val()==null){
 			$(this).css({"background-image":searchbg2});
-			mysel.init();
+			new_city.init();
 			stopBubble(event);
 		}else{
-			mysel.init2($m.s_arr2,txt,$(".search_list").eq(1));
+			new_city.init2($m.s_arr2,txt,$(".search_list"));
 		}
 	}).bind("input propertychange",function(event){
 		var txt=$(this).val();
 		if(txt==""){
-			mysel.init();
+			new_city.init();
 		}else{
-			mysel.init2($m.s_arr2,txt,$(".search_list").eq(1));
+			new_city.init2($m.s_arr2,txt,$(".search_list"));
 		}
 		stopBubble(event);
 	});
@@ -37,6 +155,7 @@ $(function(){
 		try{myScroll2.scrollToElement(".jp_"+x,10);}catch(e){}
 		stopBubble(event);
 	});
+	$(window).on("resize",function(){$m.rs()});
 });
 function ms(){
 	// 初始化
@@ -274,4 +393,54 @@ function stopBubble(e){
 		window.event.cancelBubble=true;
 	}
 	return false;
+}
+// 数组去重
+Array.prototype.unique2 = function(){
+	var n = {},r=[]; //n为hash表，r为临时数组
+	for(var i = 0; i < this.length; i++) //遍历当前数组
+	{
+		if (!n[this[i]]) //如果hash表中没有当前项
+		{
+			n[this[i]] = true; //存入hash表
+			r.push(this[i]); //把当前数组的当前项push到临时数组里面
+		}
+	}
+	return r;
+}
+// 数据去重
+function save_search(arr,type){
+	var return_arr=[];
+	var arr1=[],arr2=[],arr3=[];
+	for(var i=0,len=arr.length;i<len;i++){
+		var tar_list=arr[i]["list"];
+		for(var j=0,len1=tar_list.length;j<len1;j++){
+			var a=tar_list[j].split("@@");
+			arr1.push(a[0]);
+			arr2.push(a[1]);
+			if(type==1){
+				arr3.push(a[2]);
+			}
+		}
+	}
+	// 去重
+	arr1=arr1.unique2();
+	arr2=arr2.unique2();
+	if(type==1){
+		arr3=arr3.unique2();
+	}
+	for(var m=0,len2=arr1.length;m<len2;m++){
+		if(type==1){
+			return_arr.push({
+				"id":arr1[m],
+				"name":arr2[m],
+				"src":arr3[m]
+			})
+		}else{
+			return_arr.push({
+				"id":arr1[m],
+				"name":arr2[m],
+			})
+		}
+	}
+	return return_arr;
 }
---
+++ xian-car/wx/js/order.js
@@ -0,0 +1,122 @@
+// 滚动变量
+var myScroll1,myScroll2,myScroll3,myScroll4,myScroll5,myScroll6,myScroll7,myScroll8,myScroll9,myScroll10,myScroll11,myScroll12,myScroll13,myScroll14,myScroll15,myScroll16,myScroll17,myScroll18,myScroll19;
+// 自定义方法
+var $m={
+    // 分享链接
+    share_href: window.location.href,
+    // 搜索数据
+    s_arr1:[],
+    s_arr2:[],
+    // 图片地址前缀
+    img_url:"images/",
+    // 当前页面
+    active_scroll:1,
+    // 重新布局
+    rs: function(){
+        var a=this;
+        var limit=720;
+        var dw=document.documentElement.clientWidth;
+        var dh=document.documentElement.clientHeight;
+        var fw=dw>limit?limit:dw;
+        $("body").css({"font-size":1*fw/360+"em"});
+        if(dw>limit){
+            $(".center_box").css({"left":(dw-limit)/2+"px"});
+        };
+        $(".circular_div").each(function(){
+            a.setHeight($(this),1)
+        });
+        
+        
+        a.refreshPage(a.active_scroll);
+    },
+    // 刷新scroll
+    refreshPage: function(type){
+        if(type==1){
+            myScroll1.refresh();
+        }else if(type==2){
+            myScroll2.refresh();
+        }else if(type==3){
+            myScroll3.refresh();
+        }else if(type==4){
+            myScroll4.refresh();
+        }else if(type==5){
+            myScroll5.refresh();
+        }else if(type==6){
+            myScroll6.refresh();
+        }else if(type==7){
+            myScroll7.refresh();
+        }else if(type==8){
+            myScroll8.refresh();
+        }else if(type==9){
+            myScroll9.refresh();
+        }else if(type==10){
+            myScroll10.refresh();
+        }else if(type==11){
+            myScroll11.refresh();
+        }else if(type==12){
+            myScroll12.refresh();
+        }else if(type==13){
+            myScroll13.refresh();
+        }else if(type==14){
+            myScroll14.refresh();
+        }
+    },
+    // ajax 请求地址
+    ajax_link: ["deal_info","is_set_info"],
+    // 居中显示
+    toCenter:function(obj,par){
+        var dh=par.height();
+        var oh=obj.outerHeight();
+        obj.css({"margin-top": (dh-oh)/2-50+"px"});
+    },
+    // 图片加载
+    img_arr:[
+        "images/1.jpg",
+        "images/2.jpg",
+        "images/3.jpg",
+        "images/4.jpg",
+        "images/5.jpg",
+        "images/7.jpg",
+        "images/8.jpg",
+        "images/icon1.png",
+        "images/icon2.png",
+        "images/prod.png",
+        "images/prod1.jpg",
+        "images/txt_pic1.png",
+        "images/txt_pic2.png",
+        "images/txt_pic3.png",
+        "images/txt_pic4.png",
+        "images/txt_pic5.png",
+        "images/txt_pic6.png",
+        "images/txt_pic7.png",
+        "images/txt_pic8.png",
+    ],
+    // 设置宽高
+    setHeight:function(obj,k){
+        var ow=obj.width();
+        console.log(ow);
+        var num=parseInt(ow*k);
+        obj.height(num+"px");
+    }
+}
+$(function(){
+	// 绑定滚动
+    myScroll1=new IScroll('.page1',{mouseWheel: true,hideScrollbar: true,click: true,bounce:false});
+    myScroll2=new IScroll('.page2',{mouseWheel: true,hideScrollbar: true,click: true});
+    myScroll2=new IScroll('.page3',{mouseWheel: true,hideScrollbar: true,click: true});
+    
+
+    $m.rs();
+    
+	$(window).on("resize",function(){$m.rs()});
+});
+// 阻止事件冒泡
+function stopBubble(e){
+	if(e && e.stopPropagation){
+		e.stopPropagation();
+	}
+	else{
+		window.event.cancelBubble=true;
+	}
+	return false;
+}
